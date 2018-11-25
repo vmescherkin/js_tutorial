ba06df12c5a8bc4e0337b5f409023fd703943a5f
Vlad Mescherkin <vmescherkin@gmail.com>
2018-11-25 15:47:18 +0100

    Exercise 5 done

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("")
+}
+// Returns true for a palindrome, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
