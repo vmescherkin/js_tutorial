e62f0181290fd3120a4e486a26fcda685410279e
Vlad Mescherkin <vmescherkin@gmail.com>
2018-11-25 17:06:09 +0100

    chapter 7 done

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,46 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("")
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.translation.toLowerCase();
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+/*
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Makes the phrase LOUDER.
+  this.louder = function() {
+    let something = this.content.toUpperCase();
+    return something;
+  };
+}
+*/
